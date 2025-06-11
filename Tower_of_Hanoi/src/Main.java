06517b7279956f47e92b5607b54d95e0ac596c36
harshil1707 <164137205+harshil1707@users.noreply.github.com>
2025-06-11 21:00:07 +0530

    Add files via upload

---
+++ Tower_of_Hanoi/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    //Time Complexity: O(2^n)
+    public static void towerOfHanoi(int n, String src, String helper, String des) {
+        if (n == 1) {
+            System.out.println("transfer disk"+ n + " from " + src + " to " + des);
+            return;
+        }
+        towerOfHanoi(n-1, src, des, helper);
+        System.out.println("transfer disk"+ n + " from " + src + " to " + des);
+        towerOfHanoi(n-1, helper, src, des);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+}
