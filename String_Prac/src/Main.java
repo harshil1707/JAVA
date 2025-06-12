9d7a151cc5a915cc251c90bb220f473791b35fd2
harshil1707 <164137205+harshil1707@users.noreply.github.com>
2025-06-12 22:19:59 +0530

    Add files via upload

---
+++ String_Prac/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //string
+        //concatenate
+        String name1 = "aman";
+        String name2 = "rohit";
+        String name3 = name1 + " and " +name2;
+        System.out.println(name3);
+        //charAt
+        System.out.println(name1.charAt(0));
+        //length
+        System.out.println(name2.length());
+    }
+}
